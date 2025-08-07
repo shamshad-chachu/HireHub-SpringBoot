2045f3dc45b31ceb048fc143fa84687dc92479fd
shamshad chachu <chachushamshad0987@gmail.com>
2025-08-07 18:35:34 +0530

    cross origin modified

--- src/main/java/com/Coorg/JobSerach/ProductController.java
+++ src/main/java/com/Coorg/JobSerach/ProductController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "https://hire-hub-swart.vercel.app/")
 @RestController()
 public class ProductController {
 	@Autowired
