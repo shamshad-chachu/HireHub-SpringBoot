e2a5a88bc18c97444c05e23491dcd7688a0ead2e
shamshad chachu <chachushamshad0987@gmail.com>
2025-08-07 14:49:20 +0530

    frontend url cahnged

--- src/main/java/com/Coorg/JobSerach/ProductController.java
+++ src/main/java/com/Coorg/JobSerach/ProductController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "hire-hub-swart.vercel.app")
 @RestController()
 public class ProductController {
 	@Autowired
